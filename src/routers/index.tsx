95e88cbaec9350d616d6c48c136e8b4fc1d92154
anjiang <1508771379@qq.com>
2022-09-05 20:24:08 +0800

    feat(layout): add layout components

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,6 +1,8 @@
 import Login from "../pages/login";
 
 import { useRoutes, Navigate, RouteObject } from "react-router-dom";
+import Home from "../pages/home/index";
+import NotFound from "../components/ErrorMessage/404";
 
 const rootRouter: RouteObject[] = [
 	{
@@ -10,6 +12,23 @@ const rootRouter: RouteObject[] = [
 	{
 		path: "/login",
 		element: <Login />
+	},
+	// {
+	// 	element: <LayoutIndex name="我是参数" />,
+	// 	children: [
+	// 		{
+	// 			path: "/home",
+	// 			element: <Home />
+	// 		}
+	// 	]
+	// },
+	{
+		path: "*",
+		element: <NotFound />
+	},
+	{
+		path: "/home",
+		element: <Home />
 	}
 ];
 
